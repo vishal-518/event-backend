feffaee6bb9264f30e488b579568bba0478c42fc
MAC <mac@MACs-MBP.bbrouter>
2025-11-13 14:11:12 +0530

    first commit

--- server.js
+++ server.js
@@ -25,6 +25,10 @@ app.use("/api/eventsimg", eventImageRoutes);
 
 const PORT = process.env.PORT || 4500;
 
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
